c9a4de044a3e6b055e666bbf0daf1eb806e4b8c8
Jiyong Youn <01@0101010101.com>
2015-10-26 20:45:54 +0900

    Added more informations

--- monitor.py
+++ monitor.py
@@ -21,7 +21,12 @@ while True:
 		print('Poked ' + str(i[1]) + '(' + str(i[0]) + ') ' + str(i[2]) + ' times today')
 	print('Total: ' + str(total) + ' pokes')
 
+	c.execute('SELECT COUNT(`date`) FROM log WHERE `date` > datetime(\'' + time.strftime('%Y-%m-%d %H:%M:%S') + '\', \'-24 hours\') AND result=1;')
+	print(str((c.fetchone()[0] * 100 / 1440) / 100.0) + ' ppm for last 24 hours')
 	c.execute('SELECT COUNT(`date`) FROM log WHERE `date` > datetime(\'' + time.strftime('%Y-%m-%d %H:%M:%S') + '\', \'-1 hours\') AND result=1;')
 	print(str((c.fetchone()[0] * 100 / 60) / 100.0) + ' ppm for last 1 hour')
+	c.execute('SELECT COUNT(`date`) FROM log WHERE `date` > datetime(\'' + time.strftime('%Y-%m-%d %H:%M:%S') + '\', \'-5 minutes\') AND result=1;')
+	print(str((c.fetchone()[0] * 100 / 5) / 100.0) + ' ppm for last 5 minutes')
+
 	print('')
 	time.sleep(5)
