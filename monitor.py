41f5e2abc790f67b72c371a1a208554c021950b0
Jiyong Youn <01@0101010101.com>
2015-10-28 11:43:59 +0900

    singular plural

--- monitor.py
+++ monitor.py
@@ -18,11 +18,13 @@ while True:
 	total = 0
 	for i in result:
 		total += i[2]
-		print('Poked ' + str(i[1]) + '(' + str(i[0]) + ') ' + str(i[2]) + ' times today')
-	print('Total: ' + str(total) + ' pokes')
+		print('Poked ' + str(i[1]) + '(' + str(i[0]) + ') ' + str(i[2]) + ' time' + ('s' if (i[2] > 1) else '') + ' today')
+	print('Total: ' + str(total) + ' poke' + ('s' if (total > 1) else ''))
 
 	c.execute('SELECT COUNT(`date`) FROM log WHERE `date` > datetime(\'' + time.strftime('%Y-%m-%d %H:%M:%S') + '\', \'-24 hours\') AND result=1;')
 	print(str((c.fetchone()[0] * 100 / 1440) / 100.0) + ' ppm for last 24 hours')
+	c.execute('SELECT COUNT(`date`) FROM log WHERE `date` > datetime(\'' + time.strftime('%Y-%m-%d %H:%M:%S') + '\', \'-6 hours\') AND result=1;')
+	print(str((c.fetchone()[0] * 100 / 60) / 100.0) + ' ppm for last 6 hours')
 	c.execute('SELECT COUNT(`date`) FROM log WHERE `date` > datetime(\'' + time.strftime('%Y-%m-%d %H:%M:%S') + '\', \'-1 hours\') AND result=1;')
 	print(str((c.fetchone()[0] * 100 / 60) / 100.0) + ' ppm for last 1 hour')
 	c.execute('SELECT COUNT(`date`) FROM log WHERE `date` > datetime(\'' + time.strftime('%Y-%m-%d %H:%M:%S') + '\', \'-5 minutes\') AND result=1;')
